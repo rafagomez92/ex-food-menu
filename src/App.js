582a6dd014725c1e5e2b79aa77f14352edf0c524
rafagomez92 <rafael_gomez92@hotmail.com>
2020-02-14 00:18:21 -0600

    Modifique mi js para mostrar los productos y el carrito de compras con un solo componente llamada FoodMenu

--- src/App.js
+++ src/App.js
@@ -1,23 +1,11 @@
 import React from 'react';
-import productsData from './productsData'
-import Product from './components/Product'
-import ShoppingCart from './components/ShoppingCart'
+import FoodMenu from './components/FoodMenu'
+
 
 const App = () => {
-  const products = productsData.map((product, idx) =>
-  <Product dato={product} key={idx} />);
   return (  
     <div>
-      <div class="container">
-        <div class="row">
-          <div class="col">
-          {products}
-          </div>
-          <div class="col">
-          <ShoppingCart items={products}/>      
-          </div>
-        </div>
-    </div>
+      <FoodMenu />
   </div>      
   );
           
