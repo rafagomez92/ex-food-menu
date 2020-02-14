33c59614bb3dd1f6c79121d96c69fd108308e180
rafagomez92 <rafael_gomez92@hotmail.com>
2020-02-14 00:24:00 -0600

    Modificacion para mejorar la visa de los elementos

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,17 +1,17 @@
 import React from 'react';
 
-const Product = ({dato}) => {    
+const Product = (props) => {    
     
     return(
         <div className="container mt-3">
             <div className="card" style={{width: '18rem'}}>
-                <img src={dato.img} className="card-img-top" alt="..."/>
+                <img src={props.info.img} className="card-img-top" alt="..."/>
                 <div className="card-body bg-success text-white">
-                    <h3 className="card-title">{dato.name}</h3>
-                    <p className="card-text">{dato.description}</p>                    
+                    <h3 className="card-title">{props.info.name}</h3>
+                    <p className="card-text">{props.info.description}</p>                                       
                 </div>
                 <div class="card-footer text-muted">
-                    ${dato.price}.00
+                    ${props.info.price}.00
                 </div>
             </div>
         </div>
