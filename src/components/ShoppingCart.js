3ab3a16fa128f30579c828d9968772dfab28deb6
rafagomez92 <rafael_gomez92@hotmail.com>
2020-02-14 00:22:33 -0600

    En este js utilizo un condicional if para que muestre si el carrito de compras esta vacio o tiene elementos

--- src/components/ShoppingCart.js
+++ src/components/ShoppingCart.js
@@ -1,7 +1,8 @@
 import React from 'react';
 
-const ShoppingCart = ({items}) => {
-    let cartItems = items.length;
+const ShoppingCart = (props) => {
+    let cartItems = props.data.length;
+    
     if(cartItems > 0) {
     cartItems = <p>Total de productos seleccionados {cartItems}</p> ;
     } else {
@@ -11,11 +12,8 @@ const ShoppingCart = ({items}) => {
         
         <div className="card" style={{width: '18rem'}}>            
                 <div className="card-body">
-                <h5 className="card-title">Carrito de compras</h5>
-                {
-                    <p class="card-text">{cartItems}</p>
-                }                    
-                                                                                                                                                
+                <h5 className="card-title">Carrito de compras</h5>                
+                <p class="card-text">{cartItems}</p>                                                                                                                                                                                    
                 </div>
         </div> 
     );
